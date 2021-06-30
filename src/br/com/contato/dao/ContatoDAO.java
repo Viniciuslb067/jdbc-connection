9fec76d87004b86f34ed0e08961db5b28813946f
Viniciuslb067 <viniciuslb067@gmail.com>
2021-06-30 09:52:14 -0300

    feat: getting data from database

--- src/br/com/contato/dao/ContatoDAO.java
+++ src/br/com/contato/dao/ContatoDAO.java
@@ -6,7 +6,9 @@ import com.mysql.cj.jdbc.JdbcPreparedStatement;
 
 import java.sql.Connection;
 import java.sql.Date;
-import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContatoDAO {
 
@@ -18,7 +20,6 @@ public class ContatoDAO {
 
         try {
             conn = ConnectionFactory.createConnectionToMySql();
-
             pstm = (JdbcPreparedStatement) conn.prepareStatement(add);
 
             pstm.setString(1, contato.getName());
@@ -27,17 +28,17 @@ public class ContatoDAO {
 
             pstm.execute();
 
+            System.out.println("Contato inserido com sucesso!");
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
-                if (pstm != null) {
+                if (pstm != null)
                     pstm.close();
-                }
-
-                if(conn != null) {
+                if(conn != null)
                     conn.close();
-                }
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -45,4 +46,48 @@ public class ContatoDAO {
 
     }
 
+    public List<Contato> getContato() {
+        String read = "SELECT * FROM contatos";
+
+        List<Contato> contatos = new ArrayList<Contato>();
+
+        Connection conn = null;
+        JdbcPreparedStatement pstm = null;
+        ResultSet rset = null;
+
+        try {
+            conn = ConnectionFactory.createConnectionToMySql();
+            pstm = (JdbcPreparedStatement) conn.prepareStatement(read);
+
+            rset = pstm.executeQuery();
+
+            while (rset.next()) {
+                Contato contato = new Contato();
+
+                contato.setId(rset.getInt("id"));
+                contato.setName(rset.getString("name"));
+                contato.setAge(rset.getInt("age"));
+                contato.setCreatedAt(rset.getDate("createdAt"));
+
+                contatos.add(contato);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rset != null)
+                    rset.close();
+
+                if (pstm != null)
+                    pstm.close();
+
+                if (conn != null)
+                    conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return contatos;
+    }
 }
